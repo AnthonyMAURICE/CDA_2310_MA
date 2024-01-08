f998633775ff22ee5e731b6ca27431c39a286710
MAURICE Anthony <AMaurice@arfp.asso.fr>
2024-01-08 11:30:29 +0100

    Palindrome V2.3

--- Exercices/Palindrome/Palindrome/Program.cs
+++ Exercices/Palindrome/Palindrome/Program.cs
@@ -18,7 +18,7 @@
             {
                 int i = 0;
                 int j = _test.Length - 1;
-                while(i <= _test.Length / 2 && palin == true) // boucle sur la moitié de la chaine de caractères testée, tant que le booléen "palin" est à true
+                while(i < _test.Length / 2 && palin == true) // boucle sur la moitié de la chaine de caractères testée, tant que le booléen "palin" est à true
                 {
                     if (!_test[i].Equals(_test[j])) // si non-correspondance trouvée, passe "palin" à false
                     {
