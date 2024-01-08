daa517c16b0d0d2a6fbcb3a0654e60e2cf95d466
MAURICE Anthony <AMaurice@arfp.asso.fr>
2024-01-08 09:56:26 +0100

    Palindrome V2

--- Exercices/Palindrome/Palindrome/Program.cs
+++ Exercices/Palindrome/Palindrome/Program.cs
@@ -10,27 +10,29 @@
             Console.WriteLine(IsPalindrome(chaineDeTest));
         }
 
-
-
         public static bool IsPalindrome(string _test)
         {
+            bool palin = true;
             _test = _test.ToLower().Replace(" ", ""); // force la chaine de caractères en minuscules et supprime les espaces
             if(_test.Length > 1) // contrôle si la chaine fait plus d'un caractère
             {
-                int counter = _test.Length - 1; // compteur à partir de la fin de la chaine de caractères
-                for(int i = 0; i < _test.Length; i++) // boucle sur la chaine de caractères
+                int i = 0;
+                int j = _test.Length - 1;
+                while(i < _test.Length / 2 && palin == true) // boucle sur la moitié de la chaine de caractères testée, tant que le booléen "palin" est à true
                 {
-                    if(_test[i] != _test[counter]) // si une non-correspondance est trouvée entre l'index "i" et "counter"
+                    if (!_test[i].Equals(_test[j])) // si non-correspondance trouvée, passe "palin" à false
                     {
-                        return false; // retourne "false" 
+                        palin = false;
                     }
-                    counter--; // décrémentation du compteur pour le passage suivant dans la boucle
+                    i++;
+                    j--;
                 }
-                return true; // si aucune non-correspondance est trouvée, retourne "true"
+                return palin; // si aucune non-correspondance est trouvée, retourne "true"
             }
             else // sinon retourne "false"
             {
-                return false;
+                palin =false;
+                return palin;
             }
         }
     }
