9295bb3bdb89bb4a92f82dbe3952bac0e3feb9d1
AnthonyMAURICE <116059905+AnthonyMAURICE@users.noreply.github.com>
2024-01-25 16:24:04 +0100

    Update script.js with another method for random

--- docker/intro/www/scripts/script.js
+++ docker/intro/www/scripts/script.js
@@ -15,13 +15,26 @@ getJson().then(x => {
 
 function randomize(candidates){
     const shuffledArray = candidates.sort((a, b) => 0.5 - Math.random())
-    display(shuffledArray)
+    displayCandidats(shuffledArray)
 }
 
-function display(shuffledArray){
+function displayCandidats(shuffledArray){
     for (elem of shuffledArray){
+        // version première, avec createElement et appendChild
+        const candidateWrapper = document.createElement('div');
+        sectionWrapper.appendChild(candidateWrapper);
+        candidateWrapper.classList.add("candidat");
+        const paragraph = document.createElement('p');
+        candidateWrapper.appendChild(paragraph);
+        const datas = document.createTextNode(elem.name);
+        paragraph.appendChild(datas);
+
+        /* Autre version, avec innerHTML et backtick
+
         sectionWrapper.innerHTML += `<div class ="candidat">
                                         <p>${elem.name}</p>
                                     </div>`
+
+        */
     }
 }
