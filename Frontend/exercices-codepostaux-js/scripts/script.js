c91b64168b7cd0f5a20a8204a4937c991cd55bc4
AnthonyMAURICE <116059905+AnthonyMAURICE@users.noreply.github.com>
2024-02-09 11:35:31 +0100

    update zipcode js (still mostly WIP)

--- Frontend/exercices-codepostaux-js/scripts/script.js
+++ Frontend/exercices-codepostaux-js/scripts/script.js
@@ -1,6 +1,7 @@
 const mainContainer = document.querySelector('main')
 const searchBar = document.createElement('input')
 searchBar.setAttribute("type", "search")
+searchBar.setAttribute("list", "search")
 const button = document.createElement('button')
 const buttonText = document.createTextNode("Valider")
 button.setAttribute("type", "button")
@@ -8,13 +9,31 @@ button.appendChild(buttonText)
 mainContainer.appendChild(searchBar)
 mainContainer.appendChild(button)
 
+document.addEventListener('DOMContentLoaded', createList);
+
 async function getJson(){
     const response = await fetch('https://arfp.github.io/tp/web/javascript/02-zipcodes/zipcodes.json')
     const data = await response.json()
     return data
 }
 
-getJson().then(value => {
-    
-})
+function createList(){
+    const datalist = document.createElement('datalist');
+    datalist.setAttribute("id", "search")
+    getJson().then(values => {
+        for(let value of values){
+            const option = document.createElement('option')
+            const element = document.createTextNode(value.nomCommune)
+            option.appendChild(element)
+            datalist.appendChild(option);
+        }
+        searchBar.addEventListener("change", function(){
+            getByZipcode(values);
+        })
+    })
+    mainContainer.appendChild(datalist);
+}
 
+function getByZipcode(values){
+    return values;
+}
