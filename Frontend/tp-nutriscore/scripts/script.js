882d7921e7669a0a2f770e7227fca98aa477a57e
AnthonyMAURICE <116059905+AnthonyMAURICE@users.noreply.github.com>
2024-02-20 16:45:42 +0100

    update: finished V1 of nutriscore

--- Frontend/tp-nutriscore/scripts/script.js
+++ Frontend/tp-nutriscore/scripts/script.js
@@ -205,13 +205,19 @@ function calcNS(_value, _ns){
     return letter;
 }
 
-const headRow = document.querySelectorAll('th')
-
-for(let i = 0; i < headRow.length; i++){
-    
-}
-
-function sortingValues(_values, _key){
-    _values.sort((a, b) => a[_key] < b[_key] ? 1 : -1)
-    return _values[0]
-}
+document.querySelectorAll('th').forEach(th_elem => {
+    let asc=true
+    const index = Array.from(th_elem.parentNode.children).indexOf(th_elem)          
+    th_elem.addEventListener('click', (e) => {              
+        const arr = [... th_elem.closest("table").querySelectorAll('tbody tr')]
+        arr.sort( (a, b) => {
+            const a_val = a.children[index].innerText
+            const b_val = b.children[index].innerText                   
+            return (asc) ? a_val.localeCompare(b_val) : b_val.localeCompare(a_val)
+        })
+        arr.forEach(elem => {                   
+            th_elem.closest("table").querySelector("tbody").appendChild(elem)
+        })
+        asc = !asc
+    })
+})
