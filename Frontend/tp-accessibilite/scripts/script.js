1effc2ba44fa4388b2da6ba59f412323ade71f0a
AnthonyMAURICE <116059905+AnthonyMAURICE@users.noreply.github.com>
2024-02-29 16:02:46 +0100

    update: further accessibility features and firsts attempts of JS

--- Frontend/tp-accessibilite/scripts/script.js
+++ Frontend/tp-accessibilite/scripts/script.js
@@ -1,6 +1,14 @@
+let storage = localStorage
+
 const dialog = document.querySelector('dialog');
 const dialogBtn = document.querySelector('.btn-access')
 const closeBtn = document.querySelector('.close-dialog')
+const body = document.querySelector('body')
+const dysButton = document.querySelector('#adapted-font')
+let dysBool = storage.getItem("dys") != null ? storage.getItem("dys") : false
+let lineBool = storage.getItem("line") != null ? storage.getItem("line") : false
+let justifyBool = storage.getItem("justify") != null ? storage.getItem("justify") : false
+let imagesBool = storage.getItem("img") != null ? storage.getItem("img") : false
 
 dialogBtn.addEventListener('click', () => {
     dialog.showModal();
@@ -9,3 +17,23 @@ dialogBtn.addEventListener('click', () => {
 closeBtn.addEventListener('click', () =>{
     dialog.close()
 })
+
+if(storage.getItem("dys") != null){
+    document.addEventListener("DOMContentLoaded", function(){
+        console.log("test")
+        storage.getItem("dys") ? dysButton.checked = true : dysButton.checked = false;
+        fontDys()
+    })
+}
+
+document.querySelectorAll('.font').forEach(function(font){
+    font.addEventListener('change', fontDys)
+})
+
+function fontDys(){
+    dysBool = !dysBool
+    console.log(dysBool)
+    dysBool ? body.style.fontFamily = "dysFont": body.style.fontFamily = "Arial, Helvetica, sans-serif"
+    storage.setItem("dys", dysBool)
+}
+
