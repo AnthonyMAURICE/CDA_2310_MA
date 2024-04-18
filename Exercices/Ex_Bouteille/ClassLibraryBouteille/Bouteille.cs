7339e791aa5531072ead19b156b9c1da2753cd40
AnthonyMAURICE <116059905+AnthonyMAURICE@users.noreply.github.com>
2024-04-18 08:10:42 +0200

    Fix: some typos fixed

--- Exercices/Ex_Bouteille/ClassLibraryBouteille/Bouteille.cs
+++ Exercices/Ex_Bouteille/ClassLibraryBouteille/Bouteille.cs
@@ -31,7 +31,7 @@ namespace ClassLibraryBouteille
             return this.isOpen;
         }
 
-        public double GetqQuantityInML()
+        public double GetQuantityInML()
         {
             return this.quantityInML;
         }
@@ -62,23 +62,25 @@ namespace ClassLibraryBouteille
             }
         }
 
+        // Pour les ajouts avec paramètres, vérification si la bouteille est ouverte et que ce paramètre (_volume) est supérieur à 0
+
         public bool AddQuantity(double _volume)
         {
             if (this.isOpen && _volume > 0)
             {
-                if(this.quantityInML + _volume > this.capacityMaxInML)
+                if(this.quantityInML + _volume > this.capacityMaxInML) // Si le total quantité + volume est supérieur à la capacité
                 {
-                    this.quantityInML = this.capacityMaxInML; 
+                    this.quantityInML = this.capacityMaxInML; // on amène la quantité à la valeur de la capacité
                 }
                 else
                 {
-                    this.quantityInML += _volume;
+                    this.quantityInML += _volume; // sinon on fait l'addition comme prévu
                 }
                 return true;
             }
             else
             {
-                return false;
+                return false; // return false si la bouteille ne peut être remplie
             }
         }
 
@@ -86,45 +88,45 @@ namespace ClassLibraryBouteille
         {
             if (this.isOpen && _volume > 0)
             {
-                if (this.quantityInML - _volume < 0)
+                if (this.quantityInML - _volume < 0) // même principe à l'inverse pour la soustraction, si elle est inférieure à 0
                 {
-                    this.quantityInML = 0;
+                    this.quantityInML = 0; // on considère la bouteille comme vide
                 }
                 else
                 {
-                    this.quantityInML -= _volume;
+                    this.quantityInML -= _volume; // sinon on soustrait comme prévu
                 }
                 return true;
             }
             else
             {
-                return false;
+                return false; // return false si la bouteille ne peut être vidée
             }
         }
 
         public bool FillBottle()
         {
-            if(this.isOpen && this.quantityInML < this.capacityMaxInML)
+            if(this.isOpen && this.quantityInML < this.capacityMaxInML) // Si le contenu est inférieur à la capacité totale
             {
-                this.quantityInML = this.capacityMaxInML;
+                this.quantityInML = this.capacityMaxInML; // On amène la quantité à la valeur de la capacité
                 return true;
             }
             else
             {
-                return false;
+                return false; // return false si la bouteille ne peut être remplie
             }
         }
         
         public bool EmptyBottle()
         {
-            if(this.isOpen && this.quantityInML > 0)
+            if(this.isOpen && this.quantityInML > 0) // Si le contenu est supérieur à 0
             {
-                this.quantityInML = 0;
+                this.quantityInML = 0; // le contenu est amené à 0
                 return true;
             }
             else
             {
-                return false;
+                return false; // return false si la bouteille ne peut être vidée
             }
         }
     }
--- Exercices/Ex_Bouteille/Ex_Bouteille/Program.cs
+++ Exercices/Ex_Bouteille/Ex_Bouteille/Program.cs
@@ -11,8 +11,11 @@ namespace Ex_Bouteille
             Bouteille bouteille2 = new Bouteille(2500, 2000, false);
             Bouteille bouteille3 = new Bouteille(1000, 200, false);
 
-            bool openSuccess = bouteille1.Open();
-
+            Console.WriteLine("Est ouverte : " + bouteille2.GetOpen());
+            Console.WriteLine("Peut s'ouvrir : " + bouteille2.Open());
+            Console.WriteLine("Est ouverte : " + bouteille2.GetOpen());
+            Console.WriteLine("Peut s'ouvrir : " + bouteille2.Open());
+            Console.WriteLine("Est ouverte : " + bouteille2.GetOpen());
         }
     }  
 }
