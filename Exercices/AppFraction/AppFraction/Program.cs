f5b1b11da50171404f8dafdf4559160f4b47f7c8
AnthonyMAURICE <116059905+AnthonyMAURICE@users.noreply.github.com>
2024-04-30 15:36:54 +0200

    feat: exercice fraction

---
+++ Exercices/AppFraction/AppFraction/Program.cs
@@ -0,0 +1,38 @@
+﻿using ClassFraction;
+
+namespace AppFraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction fraction1 = new Fraction(12, 7);
+            Fraction fraction2 = new Fraction(9, 1);
+            Fraction fraction3 = new Fraction(0, 1);
+            Fraction fraction4 = new Fraction(1, 9);
+            Fraction fraction5 = new Fraction(120, -150);
+
+            Console.WriteLine(fraction1.ToString());
+            Console.WriteLine(fraction2.ToString());
+            Console.WriteLine(fraction3.ToString());
+            
+            //fraction1.Oppose();
+            fraction2.Inverse();
+
+            Console.WriteLine(fraction1.ToString());
+            Console.WriteLine(fraction2.ToString());
+
+            Console.WriteLine(fraction2.SuperieureA(fraction1));
+            Console.WriteLine(fraction2.EgaleA(fraction1));
+            Console.WriteLine(fraction2.EgaleA(fraction4));
+            Console.WriteLine(fraction5.ToString());
+
+            Fraction fraction6 = fraction1.Plus(fraction2);
+            Console.WriteLine(fraction6.ToString());
+
+            Fraction fraction7 = fraction1.Moins(fraction2);
+            Console.WriteLine(fraction7.ToString());
+
+        }
+    }
+}
---
+++ Exercices/AppFraction/ClassFraction/Fraction.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+
+namespace ClassFraction
+{
+    public class Fraction
+    {
+        private int numerateur;
+        private int denominateur;
+
+        public Fraction() : this(0, 1)
+        {
+        }
+
+        public Fraction(int _numerateur) : this(_numerateur, 1)
+        {
+            this.numerateur = _numerateur;
+        }
+
+        public Fraction(int _numerateur, int _denominateur)
+        {
+            this.numerateur = _numerateur;
+            this.denominateur = _denominateur;
+        }
+
+        public int Numerateur { get => numerateur; set => numerateur = value; }
+        public int Denominateur { get => denominateur; set => denominateur = value; }
+
+        public override string ToString()
+        {
+            double val = (double)this.Numerateur / (double)this.Denominateur;
+            bool isInteger = (double)((int)val) == (double)val;
+            return isInteger ? val.ToString() : this.Numerateur + "/" + this.Denominateur;
+        }
+
+        public void Oppose()
+        {
+            this.Numerateur *= -1;
+        }
+
+        public void Inverse()
+        {
+            (this.Numerateur, this.Denominateur) = (this.Denominateur, this.Numerateur);
+        }
+
+        protected static double Calcul(int _first, int _second)
+        {
+            return (double)_first / (double)_second;
+        }
+
+        public bool SuperieureA(Fraction _other)
+        {
+            return Calcul(this.Numerateur, this.Denominateur) > Calcul(_other.Numerateur, _other.Denominateur);
+        }
+
+        public bool EgaleA(Fraction _other)
+        {
+            return Calcul(this.Numerateur, this.Denominateur) == Calcul(_other.Numerateur, _other.Denominateur);
+        }
+
+        private int GetPgcd()
+        {
+            int a = this.Numerateur;
+            int b = this.Denominateur;
+            int pgcd = 1;
+            if (a != 0 && b != 0)
+            {
+                if (a < 0) a = -a;
+                if (b < 0) b = -b;
+                while (a != b)
+                {
+                    if (a < b)
+                    {
+                        b = b - a;
+                    }
+                    else
+                    {
+                        a = a - b;
+                    }
+                }
+                pgcd = a;
+            }
+            if (this.Numerateur < 0 ^ this.Denominateur < 0)
+            {
+                pgcd *= -1;
+            }
+            return pgcd;
+        }
+
+        public string Reduire()
+        {
+            int value = (this.Numerateur / this.GetPgcd());
+            int value2 = (this.Denominateur / this.GetPgcd());
+            if(value < 0 && value2 < 0)
+            {
+                value *= -1;
+                value2 *= -1;
+            }
+            return value.ToString() + "/" + value2.ToString();    
+        }
+
+        protected int GetFraction()
+        {
+            return (this.Denominateur / this.GetPgcd());
+        }
+
+        public Fraction Plus(Fraction _autreFraction)
+        {
+            int newNumerateur = this.Numerateur * _autreFraction.Denominateur + _autreFraction.Numerateur * this.Denominateur;
+            int newDenominateur = this.Denominateur * _autreFraction.Denominateur;
+            return new Fraction(newNumerateur, newDenominateur);
+        }
+
+        public Fraction Moins(Fraction _autreFraction)
+        {
+            int newNumerateur = this.Numerateur * _autreFraction.Denominateur - _autreFraction.Numerateur * this.Denominateur;
+            int newDenominateur = this.Denominateur * _autreFraction.Denominateur;
+            return new Fraction(newNumerateur, newDenominateur);
+        }
+
+        public Fraction Multiplie(Fraction _autreFraction)
+        {
+            int newNumerateur = this.Numerateur * _autreFraction.Numerateur;
+            int newDenominateur = this.Denominateur * _autreFraction.Denominateur;
+            return new Fraction(newNumerateur, newDenominateur);
+        }
+
+        public void Divise(Fraction _autreFraction)
+        {
+            int newNumerateur = this.Numerateur * _autreFraction.Denominateur - _autreFraction.Numerateur * this.Denominateur;
+            int newDenominateur = this.Denominateur * _autreFraction.Denominateur;
+            
+        }
+    }
+}
