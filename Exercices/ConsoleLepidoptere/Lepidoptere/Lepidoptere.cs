da2dd85f506aba45c0c00302a0a666384feb1ece
AnthonyMAURICE <116059905+AnthonyMAURICE@users.noreply.github.com>
2024-05-24 11:14:47 +0200

    update Lepidotere.cs bases

---
+++ Exercices/ConsoleLepidoptere/Lepidoptere/Lepidoptere.cs
@@ -0,0 +1,27 @@
+﻿namespace Lepidoptere
+{
+    public class Lepidoptere
+    {
+        private IStadeEvolution stadeCourant;
+
+        public Lepidoptere()
+        {
+
+        }
+
+        public void SeMetamorphoser()
+        {
+            this.stadeCourant.SeMetamorphoser();
+        }
+
+        public bool SeDeplacer()
+        {
+            return this.stadeCourant.SeDeplacer();
+        }
+
+        public override string ToString()
+        {
+            return "Lepidoptere au stade : " + this.stadeCourant.ToString();
+        }
+    }
+}
