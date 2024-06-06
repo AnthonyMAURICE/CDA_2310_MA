298a45834b830584104f9292474a10fa472b88b8
AnthonyMAURICE <116059905+AnthonyMAURICE@users.noreply.github.com>
2024-06-06 16:43:15 +0200

    update employees JS, evening commit

--- Frontend/tp-employes/script/script.js
+++ Frontend/tp-employes/script/script.js
@@ -1,4 +1,7 @@
 const table = document.querySelector('#emp-table')
+document.addEventListener('DOMContentLoaded', setData)
+let asc = true
+const salaryCol = document.querySelector('#salary').addEventListener('click', sorting)
 
 
 async function fetchData(){
@@ -7,29 +10,51 @@ async function fetchData(){
     return data
 }
 
-document.addEventListener('DOMContentLoaded', setData)
-
 function setData(){
     fetchData().then(values =>{
-        createInitialTable(values.data)
+        for(let i = 0; i < values.data.length; i++){
+            createInitialTable(values.data[i])
+        }
+        btnEnablers()
     })
 }
 
 function createInitialTable(values){
-    for(let i = 0; i < values.length; i++){
-        const row = document.createElement('tr')
-        row.appendChild(identifier(values[i]))
-        row.appendChild(getName(values[i]))
-        row.appendChild(calcEmail(values[i]))
-        row.appendChild(getSalary(values[i]))
-        row.appendChild(getYearofBirth(values[i]))
-        table.appendChild(row)
+    console.log(values)
+    const row = document.createElement('tr')
+    row.setAttribute('id', values.id)
+    row.setAttribute('class', 'rows')
+    row.appendChild(identifier(values.id))
+    row.appendChild(getName(values))
+    row.appendChild(calcEmail(values))
+    row.appendChild(getSalary(values))
+    row.appendChild(getYearofBirth(values))
+    row.appendChild(calcBtn())
+    table.appendChild(row)
+    //const deleteBtn = document.querySelectorAll('.delete')
+    
+}
+
+
+// for(let i = 0; i< deleteBtn.length; i++){
+//     deleteBtn[i].addEventListener('click', function(){
+//         delRow(deleteBtn[i])
+//     })
+// }
+
+function btnEnablers(){
+    const duplicateBtn = document.querySelectorAll('.duplicate')
+    for(let i = 0; i< duplicateBtn.length; i++){
+        duplicateBtn[i].addEventListener('click', function(){
+        duplicateRow(duplicateBtn[i])
+        })
     }
 }
 
 function identifier(value){
     const id = document.createElement('td')
-    id.textContent = value.id
+    id.setAttribute('class', value)
+    id.textContent = value
     return id
 }
 
@@ -57,4 +82,50 @@ function getYearofBirth(value){
     let date = new Date()
     year.textContent = date.getFullYear() - value.employee_age
     return year
+}
+
+function calcBtn(){
+    const buttons = document.createElement('td')
+    for(let i = 0; i < 2; i++){
+        buttons.appendChild(createBtn(i))
+    }
+    return buttons
+}
+
+function createBtn(i){
+    const btn = document.createElement('button')
+    btn.textContent = i==0? "Duplicate" : "Delete"
+    btn.setAttribute("class", btn.textContent.toLowerCase())
+    return btn
+}
+
+function delRow(_btn){
+    const lineToDelete = _btn.closest("tr")
+    lineToDelete.style.display = "none"
+}
+
+function duplicateRow(_btn){
+    const numberOfRows = document.querySelectorAll('tr')
+    const lineToDuplicate = _btn.closest("tr")
+    console.log(lineToDuplicate)
+//     const clone = lineToDuplicate.cloneNode(true)
+//     const eid = clone.children[0]
+//     eid.setAttribute('class', numberOfRows.length++)
+//     eid.textContent = numberOfRows.length++
+//     table.appendChild(clone)
+//     btnEnablers()
+}
+
+function sorting(){
+    const array = document.querySelectorAll('.rows')
+    const arr = Array.from(array)
+    if(!asc){
+        arr.sort((a, b) => a.id - b.id)
+    }else{
+        arr.sort((a, b) => b.id - a.id)
+    }
+    arr.forEach(elem => {                   
+        table.appendChild(elem)
+    })
+    asc = !asc
 }
