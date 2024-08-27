334edce6dc8aa278d0bfa30b784da9081e224f94
AnthonyMAURICE <116059905+AnthonyMAURICE@users.noreply.github.com>
2024-08-27 16:18:55 +0200

    update, still many things to fix

--- Exercices_API/TestAPI/TestAPI/wwwroot/js/Country.js
+++ Exercices_API/TestAPI/TestAPI/wwwroot/js/Country.js
@@ -1,9 +1,9 @@
 export default class Country{
-    constructor(_name, _code, _area, _continentId, _cities){
+    constructor(_name, _code, _area, _continentId){
         this.name = _name
         this.code = _code
         this.area = _area
         this.continentId = _continentId
-        this.cities = _cities
+        this.cities = []
     }
 }
--- Exercices_API/TestAPI/TestAPI/wwwroot/js/main.js
+++ Exercices_API/TestAPI/TestAPI/wwwroot/js/main.js
@@ -1,32 +1,53 @@
+import Country from './Country.js';
 import fetchData from './fetchData.js'
 
 
 const app = {
     data() {
         return {
-            countries:[]
-        }
-    },
+            countries:[],
+            activeCountry: {
+                id:0,
+                countryName:"",
+                countryArea:0,
+                countryCode:"",
+                continentId:0,
+                cities:
+                    [{id:0,
+                        cityZipCode:"",
+                        cityName:"",
+                        countryId:0
+                    }]
+                }
+            }
+        },
     async mounted() {
         let countriesFetched = await fetchData("Countries")
         if(this.countries.length == 0){
             countriesFetched.forEach(element => {
+                element = new Country(element.countryName, element.countryCode, element.countryArea, element.continentId)
             this.countries.push(element)
             });
         }
-        
+        console.log(this.countries)
     },
     methods: {
         formatFlagUrl(_countryCode, _size){
             return `https://flagsapi.com/${_countryCode}/flat/${_size}.png`
         },
-        modalDialogue(){
+        modalDialogue(country){
+            this.activeCountry = country
+            console.log(this.activeCountry)
             const dialog = document.getElementById("country-dialog");
             dialog.showModal();
+
         },
         closeDialog(){
             const dialog = document.getElementById("country-dialog");
             dialog.close()
+        },
+        getLength(_array){
+            return _array.length
         }
     }
 }
