108ee89e83c7d90f485f59b1c0ba402b5b9ed524
AnthonyMAURICE <116059905+AnthonyMAURICE@users.noreply.github.com>
2024-09-10 07:54:56 +0200

    update

--- Exercices_API/ExDto/ExDto/obj/Debug/net8.0/ExDto.AssemblyInfo.cs
+++ Exercices_API/ExDto/ExDto/obj/Debug/net8.0/ExDto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExDto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54cb8d9185803ad5f47af8f02e4c02e297182d76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+862d3ec85eaf93249ede3426672f7bda69ab6681")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExDto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExDto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
