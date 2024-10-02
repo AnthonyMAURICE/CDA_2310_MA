78e2abd3080e6b661a65ba95feec6659accf97e8
AnthonyMAURICE <116059905+AnthonyMAURICE@users.noreply.github.com>
2024-10-02 15:02:33 +0200

    feat: first WPF Ex

---
+++ WPF/ExWPF/ExWPF/ViewModels/ControlViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExWPF.ViewModels
+{
+    public class ControlViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private string name;
+
+        private DateTime date;
+
+        private string formatedDate;
+
+        private double amount;
+
+        private string zipcode;
+
+        public string Name { get => name; set => name = value; }
+        public DateTime Date { get => date; set => date = value; }
+        public double Amount { get => amount; set => amount = value; }
+        public string Zipcode { get => zipcode; set => zipcode = value; }
+        public string FormatedDate { get => this.date.ToShortDateString(); set => formatedDate = value; }
+    }
+}
